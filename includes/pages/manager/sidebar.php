d957e3fc5143eea72f4d99c9fefcd2bc9f345ad6
Nikhil Nagdev <nikhil.nagdev@outlook.com>
2019-09-22 19:22:13 +0530

    Added new Files

---
+++ includes/pages/manager/sidebar.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Aarti
+ * Date: 9/22/2019
+ * Time: 7:21 PM
+ */
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Aarti
+ * Date: 9/22/2019
+ * Time: 5:51 PM
+ */
+
+require_once "document_root.php";
+
+include_once "login.php";

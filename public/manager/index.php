6c3fb8406d7c6070543a8d12dc3ef29593b2fa69
Nikhil Nagdev <nikhil.nagdev@outlook.com>
2019-09-23 13:11:38 +0530

    Added login feature

--- public/manager/index.php
+++ public/manager/index.php
@@ -359,50 +359,50 @@ $empObj = new Employee();
 <script src="../../assets/js/demo.js"></script>
 <script src="../../assets/js/manager-script.js"></script>
 <script>
-    // var lineChart = document.getElementById('lineChart').getContext('2d');
-    // var myLineChart = new Chart(lineChart, {
-    //     type: 'line',
-    //     data: {
-    //         labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
-    //         datasets: [{
-    //             label: "Active Users",
-    //             borderColor: "#1d7af3",
-    //             pointBorderColor: "#FFF",
-    //             pointBackgroundColor: "#1d7af3",
-    //             pointBorderWidth: 2,
-    //             pointHoverRadius: 4,
-    //             pointHoverBorderWidth: 1,
-    //             pointRadius: 4,
-    //             backgroundColor: 'transparent',
-    //             fill: true,
-    //             borderWidth: 2,
-    //             data: [542, 480, 430, 550, 530, 453, 380, 434, 568, 610, 700, 900]
-    //         }]
-    //     },
-    //     options : {
-    //         responsive: true,
-    //         maintainAspectRatio: false,
-    //         legend: {
-    //             position: 'bottom',
-    //             labels : {
-    //                 padding: 10,
-    //                 fontColor: '#1d7af3',
-    //             }
-    //         },
-    //         tooltips: {
-    //             bodySpacing: 4,
-    //             mode:"nearest",
-    //             intersect: 0,
-    //             position:"nearest",
-    //             xPadding:10,
-    //             yPadding:10,
-    //             caretPadding:10
-    //         },
-    //         layout:{
-    //             padding:{left:15,right:15,top:15,bottom:15}
-    //         }
-    //     }
-    // });
+    var lineChart = document.getElementById('lineChart').getContext('2d');
+    var myLineChart = new Chart(lineChart, {
+        type: 'line',
+        data: {
+            labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+            datasets: [{
+                label: "Active Users",
+                borderColor: "#1d7af3",
+                pointBorderColor: "#FFF",
+                pointBackgroundColor: "#1d7af3",
+                pointBorderWidth: 2,
+                pointHoverRadius: 4,
+                pointHoverBorderWidth: 1,
+                pointRadius: 4,
+                backgroundColor: 'transparent',
+                fill: true,
+                borderWidth: 2,
+                data: [542, 480, 430, 550, 530, 453, 380, 434, 568, 610, 700, 900]
+            }]
+        },
+        options : {
+            responsive: true,
+            maintainAspectRatio: false,
+            legend: {
+                position: 'bottom',
+                labels : {
+                    padding: 10,
+                    fontColor: '#1d7af3',
+                }
+            },
+            tooltips: {
+                bodySpacing: 4,
+                mode:"nearest",
+                intersect: 0,
+                position:"nearest",
+                xPadding:10,
+                yPadding:10,
+                caretPadding:10
+            },
+            layout:{
+                padding:{left:15,right:15,top:15,bottom:15}
+            }
+        }
+    });
     initializeRatings();
 
     function initializeRatings() {
